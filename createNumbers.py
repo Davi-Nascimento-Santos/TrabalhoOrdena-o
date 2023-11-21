f475fd4471ae125c83f3361c59355ab4f205781d
Davi Nascimento <davinascimento19072001@gmail.com>
2023-11-21 16:06:49 -0300

    Create the file .csv

--- createNumbers.py
+++ createNumbers.py
@@ -1,10 +1,13 @@
 import csv
+from random import randint
 
 # Lista de números de 0 a 199999
-numeros = list(range(200000))
+numeros = list()
+for i in range(199999, -1, -1):
+    numeros.append(i)
 
 # Nome do arquivo CSV
-nome_arquivo = 'numeros.csv'
+nome_arquivo = 'numerosDecrescente.csv'
 
 # Escrever os números no arquivo CSV
 with open(nome_arquivo, 'w', newline='') as csvfile:
